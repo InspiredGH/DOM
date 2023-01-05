d60f7814ef3ba7a11fb2ba1e76e0b628b40faf74
InspiredGH <solomon.asante62@gmail.com>
2023-01-05 07:43:26 +0000

    comment

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // document.querySelector("h1").innerHTML =  "Good-Bye";
 
-document.querySelector("ul").lastElementChild.innerHTML = "Solomon";
+// document.querySelector("ul").lastElementChild.innerHTML = "Solomon";
