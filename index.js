a962fb5b46adcb165adf240b810a8e133e998777
InspiredGH <solomon.asante62@gmail.com>
2023-01-06 19:54:32 +0000

    adding a class to a html element
    adding a class to a html element
    using javascript
    
    document.querySelector("button").classList.add("huge");

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // document.querySelector("h1").innerHTML =  "Good-Bye";
 
 // document.querySelector("ul").lastElementChild.innerHTML = "Solomon";
+
+document.querySelector("button").classList.add("huge");
+
+// document.querySelector("button").classList.toggle("huge");
+
