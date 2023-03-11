532ceb77015a77c9b96b52ffff8a8a9c2d488970
InspiredGH <solomon.asante62@gmail.com>
2023-03-11 07:10:34 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// document.querySelector("h1").innerHTML =  "Good-Bye";
+ document.querySelector("h1").innerHTML =  "Good-Bye";
 
 // document.querySelector("ul").lastElementChild.innerHTML = "Solomon";
 
